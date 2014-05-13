e9769c7c65f011db08e355b1cf28d4022f4d6edf
ralle030583 <ralle030583@googlemail.com>
2014-05-13 21:07:19 +0200

    mod

---
+++ src/main/java/mcDams/McDamsMod.java
@@ -0,0 +1,20 @@
+package mcDams;
+
+import net.minecraft.init.Blocks;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = McDamsMod.MODID, version = McDamsMod.VERSION)
+public class McDamsMod
+{
+    public static final String MODID = "mcDams";
+    public static final String VERSION = "0.0.1";
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+		// some example code
+        System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
+    }
+}
