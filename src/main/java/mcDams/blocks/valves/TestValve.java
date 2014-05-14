1bd58a7b89ea15f63ddb7b9328b24602a3ad8927
ralle030583 <ralle030583@googlemail.com>
2014-05-15 00:09:54 +0200

    orientation

--- src/main/java/mcDams/blocks/valves/TestValve.java
+++ src/main/java/mcDams/blocks/valves/TestValve.java
@@ -11,7 +11,7 @@ import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import net.minecraftforge.common.util.ForgeDirection;
 
-public class TestValve extends Block {
+public class TestValve extends Block{
 
 	protected TestValve() {
 		super(Material.rock);
@@ -35,17 +35,14 @@ public class TestValve extends Block {
 	public TileEntity createTileEntity(World world, int metadata) {
 		return new TileDamValve();
 	}
-
-	public void onBlockPlacedBy(World world, int x, int y, int z,
-			EntityLivingBase player, ItemStack stack) {
-		int rotation = ((MathHelper
-				.floor_double((double) (player.rotationYaw * 4.0F / 360.0F) + 0.5D) & 3) + 2) % 4;
-
+	
+	public void onBlockPlacedBy(World world, int x, int y, int z, EntityLivingBase player, ItemStack stack){
+		// thats stolen from k4unl.minecraft.Hydraulicraft 
+		int rotation = ((MathHelper.floor_double((double)(player.rotationYaw * 4.0F / 360.0F) + 0.5D) & 3) + 2) % 4;
 		ForgeDirection facing = ForgeDirection.getOrientation(rotation);
-
 		TileEntity entity = world.getTileEntity(x, y, z);
-		if (entity != null && entity instanceof TileDamValve) {
-			((TileDamValve) entity).setOrientation(facing);
+		if(entity != null && entity instanceof TileDamValve){
+			((TileDamValve)entity).setOrientation(facing);
 		}
 	}
 }
