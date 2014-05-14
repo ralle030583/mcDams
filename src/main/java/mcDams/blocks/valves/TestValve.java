04c0440e741c403eed571f33a1347effbc72edfa
kentben100 <Ben>
2014-05-14 22:02:15 +0100

    2

---
+++ src/main/java/mcDams/blocks/valves/TestValve.java
@@ -0,0 +1,12 @@
+package mcDams.blocks.valves;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+public class TestValve extends Block{
+
+	protected TestValve() {
+		super(Material.rock);
+	}
+
+}
