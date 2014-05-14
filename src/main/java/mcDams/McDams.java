502b05cd3c6c4cca9a1fdd06528fa4c441fb59da
kentben100 <Ben>
2014-05-14 19:48:09 +0100

    block 2

--- src/main/java/mcDams/McDams.java
+++ src/main/java/mcDams/McDams.java
@@ -5,25 +5,36 @@ import javax.print.attribute.standard.MediaSize.Engineering;
 import buildcraft.api.fuels.IronEngineCoolant;
 import buildcraft.api.fuels.IronEngineCoolant.Coolant;
 import buildcraft.api.power.PowerHandler;
+import mcDams.blocks.TestDam;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fluids.Fluid;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.registry.GameRegistry;
 
 @Mod(modid = McDams.MODID, version = McDams.VERSION, name="Dams")
 public class McDams
 {
-    public static final String MODID = "mcDams";
+    public static final String MODID = "mcdams";
     public static final String VERSION = "0.0.1";
+    
+    
+    public static Block testDamBlock;
     
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
-		// some example code
+
         System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
-        // kentben was here
-        //
-         
+
+ 
+        testDamBlock = new TestDam().setBlockName("testDam");
+        
+		GameRegistry.registerBlock(testDamBlock,  "testDamblockname");
+
+      
     }
 }
--- src/main/java/mcDams/blocks/TestDam.java
+++ src/main/java/mcDams/blocks/TestDam.java
@@ -18,77 +18,63 @@ import net.minecraft.world.World;
 
 public class TestDam extends Block {
 
-	
+	public TestDam() {
+		super(Material.rock);
+		setBlockName("tutorialBlock");
+		setCreativeTab(CreativeTabs.tabBlock);
+	}
 
-	
-	
-	 public TestDam()
-	    {
-	        super(Material.rock);
-	        setBlockName("tutorialBlock");
-	        setCreativeTab(CreativeTabs.tabBlock);
-	    }
-	 
-	    @Override
-	    public int damageDropped(int par1)
-	    {
-	        return par1;
-	    }
-	 
-	    @SideOnly(Side.CLIENT)
-	    private IIcon[] icons;
-	 
-	    @SideOnly(Side.CLIENT)
-	    @Override
-	    public void registerBlockIcons(IIconRegister par1IconRegister)
-	    {
-	        icons = new IIcon[4];
-	 
-	        for (int i = 0; i < icons.length; i++)
-	        {
-	            icons[i] = par1IconRegister.registerIcon(McDams.MODID + ":" + (this.getUnlocalizedName().substring(5)) + i);
-	        }
-	    }
-	 
-	    @SideOnly(Side.CLIENT)
-	    @Override
-	    public IIcon getIcon(int par1, int par2)
-	    {
-	        switch (par2)
-	        {
-	        case 0:
-	            return icons[0];
-	        case 1:
-	        {
-	            switch (par1)
-	            {
-	            case 0:
-	                return icons[1];
-	            case 1:
-	                return icons[2];
-	            default:
-	                return icons[3];
-	            }
-	        }
-	        default:
-	        {
-	            System.out.println("Invalid metadata for " + this.getUnlocalizedName());
-	            return icons[0];
-	        }
-	        }
-	    }
-	 
-	    @SuppressWarnings({ "unchecked", "rawtypes" })
-	    @SideOnly(Side.CLIENT)
-	    @Override
-	    public void getSubBlocks(Item par1, CreativeTabs par2CreativeTabs, List par3List)
-	    {
-	        for (int i = 0; i < 2; i++)
-	        {
-	            par3List.add(new ItemStack(par1, 1, i));
-	        }
-	    }
+	@Override
+	public int damageDropped(int par1) {
+		return par1;
+	}
 
-	
-	
+	@SideOnly(Side.CLIENT)
+	private IIcon[] icons;
+
+	@SideOnly(Side.CLIENT)
+	@Override
+	public void registerBlockIcons(IIconRegister par1IconRegister) {
+		icons = new IIcon[4];
+
+		for (int i = 0; i < icons.length; i++) {
+			icons[i] = par1IconRegister.registerIcon(McDams.MODID + ":"
+					+ (this.getUnlocalizedName().substring(5)) + i);
+		}
+	}
+
+	@SideOnly(Side.CLIENT)
+	@Override
+	public IIcon getIcon(int par1, int par2) {
+		switch (par2) {
+		case 0:
+			return icons[0];
+		case 1: {
+			switch (par1) {
+			case 0:
+				return icons[1];
+			case 1:
+				return icons[2];
+			default:
+				return icons[3];
+			}
+		}
+		default: {
+			System.out.println("Invalid metadata for "
+					+ this.getUnlocalizedName());
+			return icons[0];
+		}
+		}
+	}
+
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	@SideOnly(Side.CLIENT)
+	@Override
+	public void getSubBlocks(Item par1, CreativeTabs par2CreativeTabs,
+			List par3List) {
+		for (int i = 0; i < 2; i++) {
+			par3List.add(new ItemStack(par1, 1, i));
+		}
+	}
+
 }
