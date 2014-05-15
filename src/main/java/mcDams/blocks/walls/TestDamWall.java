095c78f9e0bb0df71cfead994c1bc1cb0a5e5c5e
kentben100 <Ben>
2014-05-15 20:35:03 +0100

    multiblock check

--- src/main/java/mcDams/blocks/walls/TestDamWall.java
+++ src/main/java/mcDams/blocks/walls/TestDamWall.java
@@ -1,10 +1,12 @@
 package mcDams.blocks.walls;
 
+import mcDams.McDams;
 import mcDams.tileEntities.TileDamValve;
 import mcDams.tileEntities.TileDamWall;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.MathHelper;
@@ -48,7 +50,208 @@ public void onBlockPlacedBy(World world, int x, int y, int z, EntityLivingBase p
 	if(entity != null && entity instanceof TileDamValve){
 		((TileDamValve)entity).setOrientation(facing);
 	}
+	
+	
+	
+	if(checkValidMultiblock(world,x,y,z)){
+		//valid multiblock
+		System.out.println("Valid multiblock");
+		
+	}
+	
+	
 }
+
+
+private boolean checkValidMultiblock (World world, int x, int y, int z){
+	
+	if(isNextToWall(world,x,y,z)){
+		
+		if(isValidLayout(world,x,y,z)){
+	return true;
+		}
+	}
+		return false;
+
+	
+	
+}
+
+
+
+private boolean isNextToWall(World world, int x, int y, int z) {
+	
+	
+
+	
+	Block testblock;
+	
+	
+	//above
+
+	if (checkBlockMatch( world,  x,  y+1,  z)){
+		return true;	
+	}
+	//below
+	if (checkBlockMatch( world,  x,  y-1,  z)){
+		return true;	
+	}
+	//X+1
+	if (checkBlockMatch( world,  x+1,  y,  z)){
+		return true;	
+	}
+	//X-1
+	if (checkBlockMatch( world,  x-1,  y,  z)){
+		return true;	
+	}
+	//Y+1
+	if (checkBlockMatch( world,  x,  y+1,  z)){
+		return true;	
+	}
+	//Y-1
+	if (checkBlockMatch( world,  x,  y-1,  z)){
+		return true;	
+	}
+
+	return false;	
+}
+
+
+private boolean checkBlockMatch(World world, int x, int y, int z){
+	Block testblock = world.getBlock(x, y, z);
+	//if (testblock.getUnlocalizedName().equals(this.getUnlocalizedName())){
+	String tempMe = testblock.getUnlocalizedName();
+	String tempYou = this.getUnlocalizedName();
+	if (tempMe.equals(tempYou)){
+		return true;
+	}
+	//check for valve, maybe not a good way to do it in this function
+	if (testblock.getUnlocalizedName() == McDams.testValveBlock.getUnlocalizedName()){
+		return true;
+	}
+	return false;
+	
+	
+}
+
+ 
+
+
+
+private boolean isValidLayout(World world, int x, int y, int z) {
+	
+
+	
+	
+	//3x2 check only for testing
+	// X axis
+	
+	//Am i on the top row?
+	
+	boolean topRow = false;
+	int topPossition = 0;
+	
+	
+	if (checkBlockMatch(world,x,y-1,z)){
+		// there is a valid block below - I am on top row
+		
+		
+		
+		if (checkBlockMatch(world,x-1,y,z)){
+			// there is a valid on x - 1 - there is a block to my left
+			
+			if (checkBlockMatch(world,x-2,y,z)){
+				// there is a valid block x-2 - I am top right	- top row valid
+				topRow = true;
+				topPossition = 2;
+			}			
+			
+			if (checkBlockMatch(world,x+1,y,z)){
+				// there is a valid block x+1 - I am top middle - top row valid
+				topRow = true;
+				topPossition = 1;
+			}
+	
+		}	
+		
+		
+		if (checkBlockMatch(world,x+1,y,z)){
+			// there is a valid on x + 1 - there is a block to my right
+			
+			if (checkBlockMatch(world,x+2,y,z)){
+				// there is a valid block x+2 - I am top left	- top row valid
+				topRow = true;
+				topPossition = 0;
+			}			
+			
+			if (checkBlockMatch(world,x-1,y,z)){
+				// there is a valid block x-1 - I am top middle - top row valid
+				topRow = true;
+				topPossition = 1;
+			}
+	
+		}	
+		
+		
+		
+		
+	}
+	
+	
+	
+	//if there was a valid top row then check for valid bottom row
+	
+	if (topRow){
+		//get the location of bottom left block
+		int bleft = x - topPossition;
+		
+		//maybe clean these 3 into a better if
+		
+		//check first block is me
+		if (checkBlockMatch(world,bleft,y-1,z)){
+			
+			
+			//first block ok, is middle block ok?
+			if (checkBlockMatch(world,bleft+1,y-1,z)){
+				
+				
+				//first 2 block ok, is last block ok?
+				if (checkBlockMatch(world,bleft+2,y-1,z)){
+					
+					
+					//all fine so valid possition
+					
+					return true;
+					
+					
+					
+
+				}
+				
+				
+
+			}
+			
+			
+
+		}	
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	return false;
+	
+}
+
+
+
 
 
 }
